2b1608713290c5bce2b1691b253f4bee7a7ec4bb
MLNOP <victordianamlnop@gmail.com>
2023-11-03 10:41:22 +0100

    feat(update): checker update

--- includes/update-checker.php
+++ includes/update-checker.php
@@ -1,215 +1,152 @@
 <?php
 
-defined( 'ABSPATH' ) || exit;
+class mishaUpdateChecker {
 
-if (!class_exists('mishaUpdateChecker')) {
+    public $plugin_datas;
+    public $plugin_slug;
+    public $version;
+    public $cache_key;
+    public $cache_allowed;
 
-    class mishaUpdateChecker {
+    public function __construct() {
+        $this->plugin_datas = get_plugin_data(HMB_BLOCKS_PATH . HMB_BLOCKS_BASE_NAME . '.php');
+        $this->plugin_slug = HMB_BLOCKS_BASE_NAME;
+        $this->version = $this->plugin_datas['Version'];
+        $this->cache_key = 'misha_custom_upd';
+        $this->cache_allowed = false;
 
-        public $plugin_datas;
-        public $plugin_slug;
-        public $version;
-        public $cache_key;
-        public $cache_allowed;
+        add_filter('plugins_api', [$this, 'info'], 20, 3);
+        add_filter('site_transient_update_plugins', [$this, 'update']);
+        add_action('upgrader_process_complete', [$this, 'purge'], 10, 2);
+    }
 
-        public function __construct() {
-            if (!function_exists('get_plugin_data')) {
-                require_once ABSPATH . 'wp-admin/includes/plugin.php';
-            }
+    public function request() {
 
-            $this->plugin_datas = get_plugin_data(HMB_BLOCKS_PATH . HMB_BLOCKS_BASE_NAME . '.php');
-            $this->plugin_slug = HMB_BLOCKS_BASE_NAME;
-            $this->version = $this->plugin_datas['Version'];
-            $this->cache_key = 'misha_custom_upd';
-            $this->cache_allowed = false;
+        $remote = get_transient($this->cache_key);
 
-            add_filter( 'plugins_api', [$this, 'info'], 20, 3 );
-            add_filter( 'site_transient_update_plugins', [$this, 'update'] );
-            add_action( 'upgrader_process_complete', [$this, 'purge'], 10, 2 );
+        if (!$remote || !$this->cache_allowed) {
 
-        }
-
-        public function request() {
-            $remote = get_transient( $this->cache_key );
-
-            if ( false === $remote || !$this->cache_allowed ) {
-
-                $remote = wp_remote_get(
-                    'https://github.com/MLNOP/holdmyblocks/releases/latest',
-                    [
-                        'timeout' => 10,
-                        'headers' => [
-                            'Accept' => 'application/json'
-                        ]
+            $remote = wp_remote_get(
+                'https://github.com/MLNOP/holdmyblocks/releases/latest',
+                [
+                    'timeout' => 10,
+                    'headers' => [
+                        'Accept' => 'application/json'
                     ]
-                );
+                ]
+            );
 
-                if (
-                    is_wp_error( $remote )
-                    || 200 !== wp_remote_retrieve_response_code( $remote )
-                    || empty( wp_remote_retrieve_body( $remote ) )
-                ) {
-                    return false;
-                }
-
-                set_transient( $this->cache_key, $remote, DAY_IN_SECONDS );
-
-            }
-
-            $remote = json_decode( wp_remote_retrieve_body( $remote ) );
-
-            return $remote;
-
-        }
-
-        function info( $res, $action, $args ) {
-
-            // print_r( $action );
-            // print_r( $args );
-
-            // do nothing if you're not getting plugin information right now
-            if ( 'plugin_information' !== $action ) {
-                return $res;
-            }
-
-            // do nothing if it is not our plugin
-            if ( $this->plugin_slug !== $args->slug ) {
-                return $res;
-            }
-
-            // get updates
-            $remote = $this->request();
-
-            if ( !$remote ) {
-                return $res;
-            }
-
-            $res = new stdClass();
-
-            $res->name = $remote->name;
-            $res->slug = $remote->slug;
-            $res->version = $remote->tag_name;
-            $res->tested = $remote->tested;
-            $res->requires = $remote->requires;
-            $res->author = $remote->author;
-            $res->author_profile = $remote->author_profile;
-            $res->download_link = $remote->download_url;
-            $res->trunk = $remote->download_url;
-            $res->requires_php = $remote->requires_php;
-            $res->last_updated = $remote->last_updated;
-
-            $res->sections = [
-                'description' => $remote->sections->description,
-                'installation' => $remote->sections->installation,
-                'changelog' => $remote->sections->changelog
-            ];
-
-            if ( !empty( $remote->banners ) ) {
-                $res->banners = [
-                    'low' => $remote->banners->low,
-                    'high' => $remote->banners->high
-                ];
-            }
-
-            return $res;
-
-        }
-
-        public function update( $transient ) {
-
-            if ( empty($transient->checked ) ) {
-                return $transient;
-            }
-
-            $remote = $this->request();
-
-            // TODO : ajouter la vérification des versions de php & de Wordpress
             if (
-                $remote
-                && version_compare( $this->version, $remote->tag_name, '<' )
-                // && version_compare( $remote->requires, get_bloginfo( 'version' ), '<=' )
-                // && version_compare( $remote->requires_php, PHP_VERSION, '<' )
+                is_wp_error( $remote )
+                || 200 !== wp_remote_retrieve_response_code($remote)
+                || empty(wp_remote_retrieve_body($remote))
             ) {
-                $res = new stdClass();
-                $res->slug = $this->plugin_slug;
-                $res->plugin = $this->plugin_slug;
-                $res->new_version = $remote->tag_name;
-                // $res->tested = $remote->tested;
-                $res->package = $remote->update_url;
-
-                $transient->response[$res->plugin] = $res;
+                return false;
             }
 
+            set_transient($this->cache_key, $remote, DAY_IN_SECONDS);
+
+        }
+
+        $remote = json_decode(wp_remote_retrieve_body($remote));
+
+        return $remote;
+
+    }
+
+    function info($response, $action, $args) {
+
+        // do nothing if you're not getting plugin information right now
+        if ('plugin_information' !== $action) {
+            return $response;
+        }
+
+        // do nothing if it is not our plugin
+        if (empty($args->slug) || $this->plugin_slug !== $args->slug) {
+            return $response;
+        }
+
+        // get updates
+        $remote = $this->request();
+
+        if (!$remote) {
+            return $response;
+        }
+
+        $response = new stdClass();
+
+        // $response->name = $remote->name;
+        $response->slug = HMB_BLOCKS_BASE_NAME;
+        $response->version = $remote->tag_name;
+        // $response->tested = $remote->tested;
+        // $response->requires = $remote->requires;
+        // $response->author = $remote->author;
+        // $response->author_profile = $remote->author_profile;
+        // $response->donate_link = $remote->donate_link;
+        // $response->homepage = $remote->homepage;
+        $response->download_link = 'https://github.com/MLNOP/holdmyblocks/releases/latest';
+        $response->trunk = 'https://github.com/MLNOP/holdmyblocks/releases/latest';
+        // $response->requires_php = $remote->requires_php;
+        // $response->last_updated = $remote->last_updated;
+
+        // $response->sections = [
+        //     'description' => $remote->sections->description,
+        //     'installation' => $remote->sections->installation,
+        //     'changelog' => $remote->sections->changelog
+        // ];
+
+        // if (!empty( $remote->banners)) {
+        //     $response->banners = [
+        //         'low' => $remote->banners->low,
+        //         'high' => $remote->banners->high
+        //     ];
+        // }
+
+        return $response;
+
+    }
+
+    public function update($transient) {
+
+        if (empty($transient->checked)) {
             return $transient;
-
         }
 
-        public function purge( $upgrader, $options ) {
+        $remote = $this->request();
 
-            if (
-                $this->cache_allowed
-                && 'update' === $options['action']
-                && 'plugin' === $options['type']
-            ) {
-                // just clean the cache when new plugin version is installed
-                delete_transient( $this->cache_key );
-            }
+        // TODO : ajouter la vérification des versions de php & de Wordpress
+        if (
+            $remote
+            && version_compare($this->version, $remote->tag_name, '<')
+            // && version_compare($remote->requires, get_bloginfo('version'), '<=')
+            // && version_compare($remote->requires_php, PHP_VERSION, '<')
+        ) {
+            $response = new stdClass();
+            $response->slug = $this->plugin_slug;
+            $response->plugin = $this->plugin_slug;
+            $response->new_version = $remote->tag_name;
+            // $response->tested = $remote->tested;
+            $response->package = $remote->update_url;
 
+            $transient->response[$response->plugin] = $response;
         }
 
-    }
-
-    // new mishaUpdateChecker();
-
-}
-
-add_filter( 'site_transient_update_plugins', 'misha_push_update' );
-
-function misha_push_update( $transient ) {
-
-    if ( empty( $transient->checked ) ) {
         return $transient;
+
     }
 
-    $remote = wp_remote_get(
-        'https://github.com/MLNOP/holdmyblocks/releases/latest',
-        [
-            'timeout' => 10,
-            'headers' => [
-                'Accept' => 'application/json'
-            ]
-        ]
-    );
+    public function purge($upgrader, $options) {
 
-    if (
-        is_wp_error( $remote )
-        || 200 !== wp_remote_retrieve_response_code( $remote )
-        || empty( wp_remote_retrieve_body( $remote ) )
-    ) {
-        return false;
+        if (
+            $this->cache_allowed
+            && 'update' === $options['action']
+            && 'plugin' === $options['type']
+        ) {
+            // just clean the cache when new plugin version is installed
+            delete_transient( $this->cache_key );
+        }
+
     }
-
-    $remote = json_decode( wp_remote_retrieve_body( $remote ) );
-
-    // your installed plugin version should be on the line below! You can obtain it dynamically of course
-    if (
-        $remote
-        && version_compare( '0.0.1', $remote->tag_name, '<' )
-        // && version_compare( $remote->requires, get_bloginfo( 'version' ), '<' )
-        // && version_compare( $remote->requires_php, PHP_VERSION, '<' )
-    ) {
-        $res = new stdClass();
-        $res->slug = HMB_BLOCKS_BASE_NAME;
-        $res->plugin = HMB_BLOCKS_BASE_NAME . '/' . HMB_BLOCKS_BASE_NAME . '.php'; // it could be just YOUR_PLUGIN_SLUG.php if your plugin doesn't have its own directory
-        $res->new_version = $remote->tag_name;
-        $res->tested = '6.2';
-        $res->package = 'https://github.com' . str_replace('/tag', '/download', $remote->update_url) . '/' . HMB_BLOCKS_BASE_NAME . '.zip';
-
-        $transient->response[$res->plugin] = $res;
-
-        $transient->checked[$res->plugin] = $remote->tag_name;
-    }
-
-    return $transient;
-
 }
+
+new mishaUpdateChecker();
